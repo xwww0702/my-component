dd0128604469b7893e1b565347a8afd48f49c430
wqw <wqw15390270020@outlook.com>
2025-06-30 00:03:05 +0100

    doc

---
+++ packages/docs/.vitepress/theme/index.ts
@@ -0,0 +1,15 @@
+import DefaultTheme from "vitepress/theme";
+import { type App } from "vue";
+import MyComponent from "@qiwen72/my-component";
+import { ElementPlusContainer } from "@vitepress-demo-preview/component";
+
+import "@vitepress-preview/component/style.css";
+import "eric-ui/dist/index.css";
+
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }: { app: App }) {
+    app.component("demo-preview", ElementPlusContainer);
+    app.use(MyComponent);
+  },
+};
